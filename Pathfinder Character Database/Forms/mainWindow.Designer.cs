d186f05745ffd322bfc133a6ec7a9b8f5ffa33fd
knaudin <kevin.naudin@yumain.fr>
2020-02-05 20:01:14 +0100

    [MainWindow] Created main window and some menu strips.

---
+++ Pathfinder Character Database/Forms/mainWindow.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Pathfinder_Character_Database
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDatabaseAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newDatabaseToolStripMenuItem,
+            this.loadDatabaseToolStripMenuItem,
+            this.saveDatabaseToolStripMenuItem,
+            this.saveDatabaseAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadDatabaseToolStripMenuItem
+            // 
+            this.loadDatabaseToolStripMenuItem.Name = "loadDatabaseToolStripMenuItem";
+            this.loadDatabaseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadDatabaseToolStripMenuItem.Text = "Load database...";
+            this.loadDatabaseToolStripMenuItem.Click += new System.EventHandler(this.loadDatabaseToolStripMenuItem_Click);
+            // 
+            // saveDatabaseToolStripMenuItem
+            // 
+            this.saveDatabaseToolStripMenuItem.Name = "saveDatabaseToolStripMenuItem";
+            this.saveDatabaseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveDatabaseToolStripMenuItem.Text = "Save database";
+            // 
+            // saveDatabaseAsToolStripMenuItem
+            // 
+            this.saveDatabaseAsToolStripMenuItem.Name = "saveDatabaseAsToolStripMenuItem";
+            this.saveDatabaseAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveDatabaseAsToolStripMenuItem.Text = "Save database as...";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // newDatabaseToolStripMenuItem
+            // 
+            this.newDatabaseToolStripMenuItem.Name = "newDatabaseToolStripMenuItem";
+            this.newDatabaseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newDatabaseToolStripMenuItem.Text = "New database...";
+            this.newDatabaseToolStripMenuItem.Click += new System.EventHandler(this.newDatabaseToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Pathfinder Character Database";
+            this.Text = "Pathfinder Character Database";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveDatabaseAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newDatabaseToolStripMenuItem;
+    }
+}
+
---
+++ Pathfinder Character Database/Forms/mainWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pathfinder_Character_Database
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("[MainWindow] Loaded main window.");
+        }
+
+        private void newDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                Stream saveStream;
+
+                saveFileDialog.Filter = "csv files (*.csv)|*.csv";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Console.WriteLine("Saving the future database to: " + saveFileDialog.FileName);
+                    // To-Do handle data saving and loading in another class
+                    if ((saveStream = saveFileDialog.OpenFile()) != null)
+                    {
+                        saveStream.Close();
+                    }
+                    this.Text = "Pathfinder Character Database - " + Path.GetFileName(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void loadDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "csv files (*.csv)|*.csv";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    this.Text = "Pathfinder Character Database - " + Path.GetFileName(filePath);
+                    Console.WriteLine("Selected database is: " + filePath);
+                }
+            }
+        }
+    }
+}
